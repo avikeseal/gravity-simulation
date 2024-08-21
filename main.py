b7f93bd17164806a0e910027cf1fdeab36f85593
avikeseal <avikeseal@gmail.com>
2024-08-21 11:21:38 -0400

    imported necessary modules

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+#gravity simulation displaying two objects and the force acting between them:
+#importing necessary modules:
 
+import pygame
+import math
+import matplotlib.pylplot as plt
+import numpy as np
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
