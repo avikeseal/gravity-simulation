a0c3e3a5b7c048b73d381242ea43a06b5bf9b66b
avikeseal <avikeseal@gmail.com>
2024-08-21 16:48:53 -0400

    declared color palette for our simulation and initialized pygame

--- main.py
+++ main.py
@@ -12,3 +12,13 @@ WIDTH, HEIGHT = 1200, 870
 win = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Gravity Simulation")
 
+#initializing pygame:
+pygame.init()
+pygame.font.init()
+
+#colors:
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+BLUE = (135, 206, 250)
+YELLOW = (255, 255, 0)
