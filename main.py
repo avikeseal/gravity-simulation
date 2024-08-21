45593d57288379ffe0ce5a3f1a62190334690b63
avikeseal <avikeseal@gmail.com>
2024-08-21 16:44:42 -0400

    screen dimensions set

--- main.py
+++ main.py
@@ -6,3 +6,9 @@ import math
 import matplotlib.pylplot as plt
 import numpy as np
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+
+#screen dimensions:
+WIDTH, HEIGHT = 1200, 870
+win = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Gravity Simulation")
+
